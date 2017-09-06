fc4100ce2ce63b3b9df1bdca59cff92c606041ce
Unknown <piotr.magac@yahoo.com>
2017-09-06 13:41:51 +0100

    Update routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SearchComponent } from './components/search/search.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { PageNotFoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { UserComponent } from './components/user/user.component';
+
 const routes: Routes = [
   {
     path: '',
+    component: SearchComponent,
     children: []
+  },
+  { path: 'reports', component: ReportsComponent },
+  {
+    path: 'admin',
+    component: AdminComponent
+  },
+  {
+    path: 'user',
+    component: UserComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: []
 })
 export class AppRoutingModule { }
